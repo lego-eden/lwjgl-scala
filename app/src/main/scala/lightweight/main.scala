d7697309d29192be8b47d0207c4e371996a0d700
hampu <hampus.eden@gmail.com>
2024-08-14 22:34:02 +0200

    do the example from the lwjgl wiki

--- app/src/main/scala/lightweight/main.scala
+++ app/src/main/scala/lightweight/main.scala
@@ -5,5 +5,101 @@ import org.lwjgl.glfw.*
 import org.lwjgl.opengl.* 
 import org.lwjgl.system.*
 
-@main def main(): Unit =
-  println("Hello World!")
+import org.lwjgl.glfw.Callbacks.*
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.opengl.GL11.*
+import org.lwjgl.system.MemoryStack.*
+import org.lwjgl.system.MemoryUtil.*
+
+import scala.util.Using
+
+
+var window = NULL
+
+@main def run(): Unit =
+  println(s"Hello LWJGL ${Version.getVersion()}!")
+
+  init()
+  loop()
+
+  // Free the window callbacks and destroy the window
+  glfwFreeCallbacks(window)
+  glfwDestroyWindow(window)
+
+  // Terminate GLFW and free the error callback
+  glfwTerminate()
+  glfwSetErrorCallback(null).free()
+end run
+
+
+def init(): Unit =
+  // Setup an error callback. The default implementation
+  // will print the error message in System.err.
+  GLFWErrorCallback.createPrint(System.err).set()
+
+  // Initialize GLFW. Most GLFW functions will not work before doing this
+  if !glfwInit() then 
+    throw new IllegalStateException("Unable to initialize GLFW");
+
+  // Configure GLFW
+  window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL)
+  if window == NULL then
+    throw new RuntimeException("Failed to create the GLFW window");
+  
+  // Setup a key callback. It will be called every time a key is pressed, repeated, or released.
+  glfwSetKeyCallback(window, (window, key, scancode, action, mods) =>
+    if key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE then
+      glfwSetWindowShouldClose(window, true) // We will detect this in the rendering loop
+  )
+
+  // Get the thread stack and push a new frame
+  Using(stackPush())(stack =>
+    val pWidth = stack.mallocInt(1)  // int*
+    val pHeight = stack.mallocInt(1) // int*
+
+    // Get the window size passed to glfwCreateWindow
+    glfwGetWindowSize(window, pWidth, pHeight)
+
+    // Get the resolution of the primary monitor
+    val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor())
+    
+    // Center the window
+    glfwSetWindowPos(
+      window,
+      (vidmode.width() - pWidth.get(0)) / 2,
+      (vidmode.height() - pHeight.get(0)) / 2
+    )
+  ) // the stack farme is popped automatically
+
+  // Make the OpenGL context current
+  glfwMakeContextCurrent(window)
+  // Enable v-sync
+  glfwSwapInterval(1)
+
+  // Make the window visible
+  glfwShowWindow(window)
+end init
+
+
+def loop(): Unit =
+  // This line is critical for LWJGL's interoperation with GLFW's
+  // OpenGL context, or any context that is managed externally.
+  // LWJGL detects the context that is current in the current thread,
+  // creates the GLCapabilities instance and makes the OpenGL
+  // bindings available for use.
+  GL.createCapabilities()
+
+  // Set the clear color
+  glClearColor(1, 0, 0, 0)
+
+  // Run the rendering loop until the user has attempted to close
+  // the window or has pressed the ESCAPE key.
+  while !glfwWindowShouldClose(window) do
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) // clear the framebuffer
+
+    glfwSwapBuffers(window) // swap the color buffers
+
+    // Poll for window events. The key callback above will inly be
+    // invoked during this call.
+    glfwPollEvents()
+end loop
